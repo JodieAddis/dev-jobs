bfc5cf79bff4d1eedb1d8c24e99cf6b5356ad3aa
JodieAddis <jodie.addis.92@gmail.com>
2024-05-27 16:24:28 +0200

    adding job card component

---
+++ src/components/DevJob/DevJobCard/index.tsx
@@ -0,0 +1,26 @@
+import Heading from "../../../typographies/Heading";
+import Paragraph from "../../../typographies/Paragraph";
+
+const Component = () => {
+  return (
+    <>
+      <div className="company_job">
+        <div className="company_logo">logo</div>
+        <div>
+          <Paragraph content="5h ago * Full Time" css="job_contract" />
+        </div>
+        <Heading
+          kind="h2"
+          content="senior software engineer"
+          css={"job_name"}
+        />
+        <Paragraph content="Scoot" css="job_company_name" />
+        <a href="#" className="job_location">
+          United Kingdom
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Component;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../public/sass/style.scss";
-import App from "./App";
+import PageDevJob from "./page/PageDevJob";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PageDevJob />
   </React.StrictMode>
 );
